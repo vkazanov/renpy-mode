7073c7a3416c108468a8ae78dbc4f04e53d4b598
Vladimir Kazanov <vekazanov@gmail.com>
2025-05-06 09:47:47 +0100

    initial font lock test, TODO.org update

---
+++ test/renpy-font-lock-test.el
@@ -0,0 +1,20 @@
+;;; renpy-font-lock-test.el ---                     -*- lexical-binding: t; -*-
+
+;;; Code:
+
+(require 'ert)
+(require 'renpy)
+
+; NOTE: ert-font-lock was only introduced in Emacs 30
+(when (>= emacs-major-version 30)
+  (require 'ert-font-lock)
+  (ert-font-lock-deftest test-renpy-font-lock-basic renpy-mode
+    "
+label start:
+# <- font-lock-keyword-face
+#^^^^font-lock-keyword-face
+#     ^^^^^ font-lock-function-name-face
+    say \"Hello, World!\"
+"))
+
+;;; renpy-font-lock-test.el ends here
