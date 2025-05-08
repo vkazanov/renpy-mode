ba605f7af1ab150cc4d1945f325aeffec01ca40a
Vladimir Kazanov <vekazanov@gmail.com>
2025-05-08 18:12:24 +0100

    Basic keyword font-lock tests

--- test/renpy-font-lock-test.el
+++ test/renpy-font-lock-test.el
@@ -8,6 +8,11 @@
 ;; NOTE: ert-font-lock was only introduced in Emacs 30.
 (when (>= emacs-major-version 30)
   (require 'ert-font-lock)
+  ;; TODO: Atomic tests below should be accompanied with a single example file
+  ;; imported from Ren'py.
+
+;;;; Ren'py Keywords
+
   (ert-font-lock-deftest test-renpy-font-lock-basic renpy-mode
     "
 label start:
@@ -19,6 +24,53 @@ label start:
 label .local_label:
 #     ^^^^^^^^^^^^ font-lock-function-name-face
     pass
+")
+
+  (ert-font-lock-deftest test-renpy-font-lock-jump-call renpy-mode
+    "
+label start:
+    call random_label
+#   ^^^^ font-lock-keyword-face
+
+    jump other_chapter
+#   ^^^^ font-lock-keyword-face
+")
+
+  (ert-font-lock-deftest test-renpy-font-lock-image renpy-mode
+    "
+image eileen = \"eileen_happy.png\"
+# <- font-lock-keyword-face
+#^^^^ font-lock-keyword-face
+#     ^^^^^^ font-lock-variable-name-face
+
+image eileen happy wet = \"eileen_happy.png\"
+# <- font-lock-keyword-face
+#^^^^ font-lock-keyword-face
+#     ^^^^^^ font-lock-variable-name-face
+#            ^^^^^ font-lock-preprocessor-face
+#                  ^^^ font-lock-preprocessor-face
+")
+
+  (ert-font-lock-deftest test-renpy-font-lock-show-hide renpy-mode
+    "
+show eileen happy with dissolve
+# <- font-lock-keyword-face
+#^^^              ^^^^ font-lock-keyword-face
+
+show eileen happy at left with move
+# <- font-lock-keyword-face
+#^^^              ^^      ^^^^ font-lock-keyword-face
+
+hide eileen with dissolve
+# <- font-lock-keyword-face
+#^^^        ^^^^ font-lock-keyword-face
+")
+
+  (ert-font-lock-deftest test-renpy-font-lock-scene renpy-mode
+    "
+scene bg whitehouse with fade
+# <- font-lock-keyword-face
+#^^^^               ^^^^ font-lock-keyword-face
 "))
 
 ;;; renpy-font-lock-test.el ends here
