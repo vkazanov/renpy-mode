d36a4b3cc713935f0b6a868bebe5604afc9f494d
Vladimir Kazanov <vekazanov@gmail.com>
2025-05-09 06:53:44 +0100

    More style fixes related to PR #5

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,7 +1,13 @@
 ;;; test-helper.el --- Helpers                       -*- lexical-binding: t; -*-
 
+;; NOTE: test/test-helper.el is always loaded by ert-runner prior to running
+;; unit tests. If manual test run is necessary then this file has to be loaded
+;; manually.
+
+;;; Code:
+
 (defmacro with-temp-buffer-str (str &rest body)
-  "Eval BODY within a buffer with containing STR. "
+  "Eval BODY within a buffer with containing STR."
   (declare (indent 1) (debug t))
   `(with-temp-buffer
      (insert ,str)
