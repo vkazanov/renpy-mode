98792aaa468f7ee2ef675620255b160802b5392c
Vladimir Kazanov <vekazanov@gmail.com>
2025-06-19 07:56:46 +0100

    Fixed broken symbol/image bounds checking.

--- test/renpy-capf-test.el
+++ test/renpy-capf-test.el
@@ -143,6 +143,17 @@ show |
 "
   ("eileen happy" "black" "bg tiled"))
 
+(renpy-test-capf show-image-2
+  "
+image test1:
+    \"test_one\"
+
+image test2:
+    \"test_two\"
+
+show t|"
+  ("test1" "test2"))
+
 (renpy-test-capf show-image-tag
   "
 image eileen = \"eileen.png\"
@@ -282,5 +293,73 @@ scene amg at transform1, |
 
 
 "  ("left_to_right" "animated_ariana_disp"))
+
+;;; Symbol bounds tests.
+
+(renpy-test-bounds test-renpy-find-symbol-bounds
+  "   test_symbol   "
+  "test"
+  (lambda () (renpy--find-symbol-bounds "w_"))
+  "test_symbol")
+
+(renpy-test-bounds test-renpy-find-symbol-bounds-2
+  "   test_symbol   "
+  "test"
+  (lambda () (renpy--find-symbol-bounds "w"))
+  "test")
+
+(renpy-test-bounds test-renpy-find-symbol-bounds-leading-punctuation
+  "   _leadingSymbol   "
+  "_leading"
+  (lambda () (renpy--find-symbol-bounds "w_"))
+  "_leadingSymbol")
+
+(renpy-test-bounds test-renpy-find-symbol-bounds-single-char
+  "x"
+  "x"
+  (lambda () (renpy--find-symbol-bounds "w_"))
+  "x")
+
+(renpy-test-bounds test-renpy-find-image-bounds-1
+  "show example image"
+  "example"
+  renpy--find-image-bounds
+  "example image")
+
+(renpy-test-bounds test-renpy-find-image-bounds-2
+  "show example image with t"
+  "example"
+  renpy--find-image-bounds
+  "example image")
+
+(renpy-test-bounds test-renpy-find-image-bounds-3
+  "show example image as smth"
+  "example"
+  renpy--find-image-bounds
+  "example image")
+
+(renpy-test-bounds test-renpy-find-image-bounds-keyword-eol
+  "show "
+  " "
+  renpy--find-image-bounds
+  "")
+
+(renpy-test-bounds test-renpy-find-image-bounds-char
+  "x"
+  ""
+  renpy--find-image-bounds
+  "x")
+
+(renpy-test-bounds test-renpy-find-image-bounds-chars
+  "x y z"
+  ""
+  renpy--find-image-bounds
+  "x y z")
+
+(renpy-test-bounds test-renpy-find-image-bounds-immediate-keyword
+  "showscene"
+  "showscene"
+  renpy--find-image-bounds
+  "showscene")
 
 ;;; renpy-capf-test.el ends here
